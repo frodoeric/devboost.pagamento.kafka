0bd86e6666641e3092665d8a2871e0501f227eb8
Eric Joseph Bezerra Costa <frodoeric@hotmail.com>
2020-09-15 07:56:17 -0300

    ajuste configuration referências

--- GestaoPagamento/Tests.Pay.TDD/Config/StartInjection.cs
+++ GestaoPagamento/Tests.Pay.TDD/Config/StartInjection.cs
@@ -1,6 +1,8 @@
 ﻿using IoC.Pay;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.IO;
 
 namespace Tests.Pay.TDD.Config
 {
@@ -12,14 +14,31 @@ namespace Tests.Pay.TDD.Config
         }
 
         public IServiceProvider ServiceProvider { get; private set; }
+        public IConfiguration Configuration { get; private set; } = StartConfiguration.Configuration;
 
         void BuildServiceProvider()
         {
             var _services = new ServiceCollection();
             _services.RegisterDbContextInMemory();
-            _services.RegisterServices(true);
+            _services.RegisterServices(Configuration, true);
+            _services.AddSingleton(x => this.Configuration);
+
+            _services.RegisterServices(Configuration, true);
 
             ServiceProvider = _services.BuildServiceProvider();
+        }
+    }
+
+    public static class StartConfiguration
+    {
+        public static IConfiguration Configuration { get; private set; }
+
+        static StartConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
         }
     }
 }
